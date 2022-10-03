cea2746bf179f3e6f6308027b02a8453f38d8860
Vanessa <mistycataj@gmail.com>
2022-10-02 20:46:30 -0400

    finished Point.java

--- src/Point.java
+++ src/Point.java
@@ -4,58 +4,57 @@ public class Point {
     private int y;
 
     //constructors
-    public Point (int x, int y) {
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public Point (int value) {
+    public Point(int value) {
         x = value;
         y = value;
     }
 
-    public Point () {
+    public Point() {
         x = 0;
         y = 0;
     }
 
     //getters
-    public int getX () {
+    public int getX() {
         return x;
     }
 
-    public int getY () {
+    public int getY() {
         return y;
     }
 
     //setters
-    public void setX (int newX) {
+    public void setX(int newX) {
         x = newX;
     }
 
-    public void setY (int newY) {
+    public void setY(int newY) {
         y = newY;
     }
 
-    public String coordinate () {
+    public String coordinate() {
         return "(" + x + ", " + y + ")";
     }
 
-    public String quadrant () {
-        if (x > 0 && y > 0) {
+    public String quadrant() {
+        if (x == 0 && y == 0) {
+            return "origin";
+        } else if (x > 0 && y > 0) {
             return "I";
-        }
-
-        if (x < 0 && y > 0) {
+        } else if (x < 0 && y > 0) {
             return "II";
-        }
-
-        if (x < 0 && y < 0) {
+        } else if (x < 0 && y < 0) {
             return "III";
+        } else if (x > 0 && y < 0) {
+            return "IV";
+        } else {
+            return "on an axis";
         }
 
-        if (x > 0 && y < 0) {
-            return "IV";
-        }
     }
 }
